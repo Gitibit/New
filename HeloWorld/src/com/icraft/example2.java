7fc998dee227037bc8cdb61cecb4add3d5bb994c
Yihune <mekoyayihune@gmail.com>
2022-02-27 20:01:33 -0800

    another sample file commited from eclipse.

---
+++ HeloWorld/src/com/icraft/example2.java
@@ -0,0 +1,11 @@
+package com.icraft;
+
+public class example2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Helo World");
+		System.out.println("Helo World");
+	}
+
+}
