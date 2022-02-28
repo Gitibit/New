361e36655b407b85e2c38d35a10602caab34cc39
Yihune <mekoyayihune@gmail.com>
2022-02-28 15:10:10 -0800

    commit

--- HeloWorld/src/com/icraft/example2.java
+++ HeloWorld/src/com/icraft/example2.java
@@ -5,8 +5,8 @@ public class example2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Helo World");
-		System.out.println("Helo World");
 		System.out.println("This is fun to work with eclipse");
-	}
+	}	System.out.println ("This is the second fun to work with eclipse")
+	
 
 }
