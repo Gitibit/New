2a0baeae16da15f721207f4232399dd9f0853d6e
Yihune <mekoyayihune@gmail.com>
2022-02-28 16:51:48 -0800

    updated from eclipse

---
+++ New/HeloWorld/src/com/icraft/example2.java
@@ -0,0 +1,12 @@
+package com.icraft;
+
+public class example2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Helo World");
+		System.out.println("This is fun to work with eclipse");
+	}	System.out.println ("This is the second fun to work with eclipse")
+	
+
+}
